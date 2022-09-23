7ee2ea39c69fef12eacf0999a09a7061a7ef00f4
seyitarslanturk@gmail.com <seyitarslanturk@gmail.com>
2022-09-23 18:19:30 +0300

    I send my homework. But While I was making, I checked another students homework. I made to copy paste some parts their homeworks. I know that wrong method. But I just learn.

---
+++ src/App.vue
@@ -0,0 +1,93 @@
+<script setup>
+import { onMounted } from "vue";
+import BootcampLogo from "./assets/svg/BootcampLogo.vue"
+import fecthingData from "./servicesData/fetchData.js";
+import {ref} from "vue"
+import StudentsByGroup from "./components/StudentsByGroup.vue";
+
+const filteredByAssisstantData = ref([]);
+const groupNames = ref([]) // assigned student values to ref in order to use it later
+
+function getGroupNames (data) {
+	const groups = data.map(element  => element.group);
+	let unique = [...new Set(groups)];
+
+
+	groupNames.value = unique
+	console.log(unique);
+
+	return unique
+}
+function getElementByGroupName(groupName){
+
+	let groupData = filteredByAssisstantData.filter(element => element.group == groupName)
+
+	let newGroupData = groupData.students
+
+	return newGroupData;
+
+}
+function studentsByGroup(data) {  // this function return students as required in order to use them.
+	let asistants = data.filter(student => student.type);
+	let students = data.filter(student => !student.type);
+	asistants = asistants.map(asistant => {
+		const filteredStudents = students.filter(
+			(student) => student.group === asistant.group
+		);
+		let newfiltered = filteredStudents.map(name => name.name)
+
+		return {
+			group: asistant.group,
+			asistants: asistant.name,
+			students: newfiltered
+		};
+	});
+
+	console.log(asistants);
+	filteredByAssisstantData.value = asistants // assigned this value to ref.
+	return asistants;
+}
+
+// fetchingData() function is imported for readability. Because it is async function, we use await 
+
+onMounted(async() => {
+	const data  = await fecthingData(); 
+	studentsByGroup(data);
+	getGroupNames (data)
+})
+</script>
+
+<template>
+	<div>
+		<BootcampLogo />
+		<br />
+		<a href="https://vitejs.dev" target="_blank">
+			<img src="/vite.svg" class="logo" alt="Vite logo" />
+		</a>
+		<a href="https://vuejs.org/" target="_blank">
+			<img src="/vue.svg" class="logo vue" alt="Vue logo" />
+		</a>
+	</div>
+<StudentsByGroup :data="filteredByAssisstantData"></StudentsByGroup> 
+
+
+</template>
+
+<style lang="scss" scoped>
+.logo {
+	height: 6em;
+	padding: 1.5em;
+	will-change: filter;
+}
+
+.logo:hover {
+	filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+	filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+
+
+</style>
---
+++ src/components/StudentsByGroup.vue
@@ -0,0 +1,84 @@
+<script setup>
+const props = defineProps(["data"]);
+</script>
+<template>
+<div class="mainTemplate"></div>
+	<div id="header">
+		<h2>You are seeing students and assistant and group names at now, It has filtered in the down.</h2>
+		<hr>
+	</div>
+	<div id="container">
+		<section v-for="data in props.data"  >		
+    		<div id="first">				
+				<strong class="group">Groups Name : {{data.group}}</strong> 			
+			</div>						
+    		<div id="second">
+				<strong class="assistan">
+					Assistant Name : {{data.asistants}}
+				</strong> 		
+			</div>
+			<div id="third">
+				<div class="student" v-for="data in data.students" >
+					{{data}}
+				</div>
+			</div>
+		</section>
+	</div>
+
+	
+</template>
+<style lang="scss" scoped>
+
+#header {
+    width: 100%;
+    height: auto;
+	
+}
+
+#container {
+    width: 800 px;
+    margin: auto;
+}
+#first {
+    width: 400 px;
+    float: left;
+    height: auto;
+	text-align: center;
+	
+}
+#second {
+	margin-left: 25 px;
+    width: 375 px;
+    height: auto;
+
+}
+#third{
+    margin-left: 25 px;
+    width: 375 px;
+    height: auto;
+	text-align: center;	
+}
+.student {
+	font-style:normal;
+	border-radius: 15px;
+
+	
+}
+	
+.assistan {
+	background-color: rgb(12, 73, 31);
+	font-style: normal;
+	border-radius: 5px;
+	color: rgb(221, 224, 224);
+	text-align: center;
+}
+.group {
+	
+	font-style: normal;
+	background-color: antiquewhite;
+	color: blue;
+	border-radius: 25 px;
+	font-size:100%;
+}
+
+	</style>
